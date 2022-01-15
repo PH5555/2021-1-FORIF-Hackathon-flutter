1954e1fae7dc5025dfa2d4d177b5fcad1730205e
김동현 <47862506+PH5555@users.noreply.github.com>
2022-01-15 09:33:37 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/forif_hackthon_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.forif_hackthon_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/preparation_room.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PreparationRoom extends StatefulWidget {
+  const PreparationRoom({Key? key}) : super(key: key);
+
+  @override
+  _PreparationRoomState createState() => _PreparationRoomState();
+}
+
+class _PreparationRoomState extends State<PreparationRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
