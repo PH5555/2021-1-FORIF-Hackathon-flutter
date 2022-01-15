9f64f8df750672bacd9175f4ea8c49cb54c93ac1
jang1161 <jang020407@naver.com>
2022-01-15 16:01:43 +0900

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   height: 50,
                 ),
-                Image.asset("imgs/homebackgrnd.jpg", width: 500),
+                Image.asset("imgs/homebackgrnd.png", width: 500),
                 SizedBox(
                   height: 20,
                 ),
