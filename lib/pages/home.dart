6708e8f206758c0ba0ffd948776b318f00d2b199
jang1161 <jang020407@naver.com>
2022-01-15 13:50:08 +0900

    홈

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forif_hackthon_flutter/pages/preparation_room.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +11,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(onTap: (){Navigator.push(
+      context,
+      MaterialPageRoute(
+         builder: (context) => const PreparationRoom()));},
+      child: Container(
+        child: Scaffold(
+            backgroundColor: Color(0xff6DA95F),
+            body: Center(
+              child: Column(children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset("imgs/homebackgrnd.jpg", width: 500),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "터치하여 시작",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ]),
+            ),
+          ),
+      ),
+    );
   }
 }
