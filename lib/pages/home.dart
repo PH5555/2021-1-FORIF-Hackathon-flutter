d4f7c674e7f398b7eb13a33a9e7eda9cc5382e15
김동현 <47862506+PH5555@users.noreply.github.com>
2022-01-15 16:24:29 +0900

    중간 저장

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,20 +19,29 @@ class _HomeState extends State<Home> {
       child: Container(
         child: Scaffold(
           backgroundColor: Color(0xff6DA95F),
-          body: Center(
-            child: Column(children: [
-              SizedBox(
-                height: 50,
+          body: Stack(
+            children: [
+              Center(
+                child: Column(children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Image.asset("imgs/homebackground.png", width: 500),
+                ]),
               ),
-              Image.asset("imgs/homebackgrnd.png", width: 500),
-              SizedBox(
-                height: 20,
+              Positioned(
+                bottom: 50,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: Text(
+                      "터치하여 시작",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
-              Text(
-                "터치하여 시작",
-                style: TextStyle(color: Colors.white),
-              ),
-            ]),
+            ],
           ),
         ),
       ),
